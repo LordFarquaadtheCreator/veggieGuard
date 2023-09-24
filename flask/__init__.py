0b62b4c92021b81576eebf55fefd9075611e18c7
Kusum Gandham <koolkusum10@gmail.com>
2023-09-24 13:08:30 -0400

    hhh

--- flask/__init__.py
+++ flask/__init__.py
@@ -18,7 +18,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 DATABASE = 'columbia.db'
-UPLOAD_FOLDER = '/UPLOAD_FOLDER'
+UPLOAD_FOLDER = 'UPLOAD_FOLDER'
 
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
