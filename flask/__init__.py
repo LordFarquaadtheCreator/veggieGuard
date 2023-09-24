27bc5cc9598fd6b1d86869b82ef552b8aa3e052d
Kusum Gandham <koolkusum10@gmail.com>
2023-09-24 11:20:36 -0400

    image

--- flask/__init__.py
+++ flask/__init__.py
@@ -7,7 +7,8 @@ from pymongo.errors import ConnectionFailure
 import os
 from helpers import login_required
 from flask_session import Session
-
+from werkzeug.utils import secure_filename
+UPLOAD_FOLDER = 'photos'
 #account_sid = os.environ['AC8ef87725499512171f849e7a4beb8dc0']
 
 app = Flask(__name__)
@@ -17,6 +18,9 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 DATABASE = 'columbia.db'
+UPLOAD_FOLDER = '/UPLOAD_FOLDER'
+
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -30,6 +34,31 @@ def close_db(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
+@app.route('/', methods=['GET','POST'])
+def upload_file():
+    if request.method == 'POST':
+        # Check if the POST request has a file part
+        if 'file' not in request.files:
+            return redirect(request.url)
+
+        file = request.files['image-upload']
+
+        # If the user does not select a file, the browser sends an empty file
+        if file.filename == '':
+            return redirect(request.url)
+
+        if file:
+            # Ensure the filename is secure (remove unsafe characters)
+            filename = secure_filename(file.filename)
+
+            # Save the uploaded file to the specified folder
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            # Optionally, you can return a message indicating the upload was successful
+            return 'File uploaded successfully'
+    else: 
+        return render_template('mainpage.html')
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -94,12 +123,12 @@ def login():
     return render_template('login.html')
 
 
-@app.route('/')
-def mainpage():
-    if 'user_id' not in session:
-        return redirect(url_for('login'))
+# @app.route('/')
+# def mainpage():
+#     if 'user_id' not in session:
+#         return redirect(url_for('login'))
     
-    return render_template('mainpage.html')
+#     return render_template('mainpage.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
